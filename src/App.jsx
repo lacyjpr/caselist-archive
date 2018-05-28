20fb76762692bf35294a319e86e1913c044c238a
Jean-Paul Lacy <lacyjpr@gmail.com>
2018-05-28 13:09:39 -0700

    Edit URLs again for gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,72 +7,72 @@ const App = () => (
     </div>
     <ul>
       <li>
-        <a href="/assets/2000_list.zip" download>
+        <a href="assets/2000_list.zip" download>
           2000 list
         </a>
       </li>
       <li>
-        <a href="/assets/2001_list.zip" download>
+        <a href="assets/2001_list.zip" download>
           2001 list
         </a>
       </li>
       <li>
-        <a href="/assets/2002_list.zip" download>
+        <a href="assets/2002_list.zip" download>
           2002 list
         </a>
       </li>
       <li>
-        <a href="/assets/2003_list.zip" download>
+        <a href="assets/2003_list.zip" download>
           2003 list
         </a>
       </li>
       <li>
-        <a href="/assets/2004_list.zip" download>
+        <a href="assets/2004_list.zip" download>
           2004 list
         </a>
       </li>
       <li>
-        <a href="/assets/2004_NDT_list.zip" download>
+        <a href="assets/2004_NDT_list.zip" download>
           2004 NDT list
         </a>
       </li>
       <li>
-        <a href="/assets/2005_list.zip" download>
+        <a href="assets/2005_list.zip" download>
           2005 list
         </a>
       </li>
       <li>
-        <a href="/assets/2005_NDT_list.zip" download>
+        <a href="assets/2005_NDT_list.zip" download>
           2005 NDT list
         </a>
       </li>
       <li>
-        <a href="/assets/2006_list.zip" download>
+        <a href="assets/2006_list.zip" download>
           2006 list
         </a>
       </li>
       <li>
-        <a href="/assets/2006_NDT_list.zip" download>
+        <a href="assets/2006_NDT_list.zip" download>
           2006 NDT list
         </a>
       </li>
       <li>
-        <a href="/assets/2008_list.zip" download>
+        <a href="assets/2008_list.zip" download>
           2008 list
         </a>
       </li>
       <li>
-        <a href="/assets/2009_list.zip" download>
+        <a href="assets/2009_list.zip" download>
           2009 list
         </a>
       </li>
       <li>
-        <a href="/assets/2010_list.zip" download>
+        <a href="assets/2010_list.zip" download>
           2010 list
         </a>
       </li>
       <li>
-        <a href="/assets/2011_list.zip" download>
+        <a href="assets/2011_list.zip" download>
           2011 list
         </a>
       </li>
